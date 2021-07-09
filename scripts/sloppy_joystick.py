30c28e7ed4f3329e3a60d34555974536096da8dd
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-07-09 08:02:30 +0200

    sloppy joystick class to prevent non causal timing issues

--- scripts/sloppy_joystick.py
+++ scripts/sloppy_joystick.py
@@ -6,28 +6,41 @@ from geometry_msgs.msg import Twist
 import behavior_gates as bg
 
 
-def joystick_callback(twist):
 
-    wait_time       = np.abs(np.random.normal(0,0.04))
-    ctr_noise       = np.array([np.random.normal(0,0.01),np.random.normal(0,0.2)])
-    lin_quant_steps = []
-    ang_quant_steps = []
-    joystick_input  = np.array([twist.linear.x,twist.angular.z])
+class sloppy_joystick:
 
-    rospy.sleep(wait_time)
+    def __init__(self):
+        self.lin_quant_steps = []
+        self.ang_quant_steps = []
+        self.noise_variance  = np.array([0.01,0.2])
+        self.joystick_input  = np.zeros(2)
 
-    joystick_input = joystick_input + ctr_noise
-    if lin_quant_steps != []:
-        joystick_input[0]=np.digitize(joystick_input[0],bins=lin_quant_steps)
-    if ang_quant_steps != []:
-        joystick_input[1]=np.digitize(joystick_input[1],bins=ang_quant_steps)
+        pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        #pub = rospy.Publisher('/sloppy_joy_cmd', Twist, queue_size=1)
+        rospy.Subscriber('/joy_cmd', Twist, self.joystick_callback)
+        while not rospy.is_shutdown():
+            wait_time       = np.abs(np.random.normal(0,0.04))
+            ctr_noise       = np.array([np.random.normal(0,self.noise_variance[0]),np.random.normal(0,self.noise_variance[1])])
 
-    output = Twist()
-    output.linear.x  = max(-1,min(1,joystick_input[0]))
-    output.angular.z = max(-1.3,min(1.3,joystick_input[1]))/1.3
+            rospy.sleep(wait_time)
 
-    pub.publish(output)
-    
+            joystick_input = self.joystick_input + ctr_noise
+            if self.lin_quant_steps != []:
+                joystick_input[0]=np.digitize(joystick_input[0],bins=self.lin_quant_steps)
+            if self.ang_quant_steps != []:
+                joystick_input[1]=np.digitize(joystick_input[1],bins=sel.fang_quant_steps)
+
+            output = Twist()
+            output.linear.x  = max(-1,min(1,joystick_input[0]))
+            output.angular.z = max(-1.3,min(1.3,joystick_input[1]))/1.3
+
+            pub.publish(output)
+            
+
+    def joystick_callback(self,twist):
+        self.joystick_input  = np.array([twist.linear.x,twist.angular.z])
+
+
 
 
 
@@ -39,9 +52,7 @@ def joystick_callback(twist):
 if __name__ == '__main__':
 	try:
                 rospy.init_node("joystick_model")
-		pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-		#pub = rospy.Publisher('/sloppy_joy_cmd', Twist, queue_size=1)
-                rospy.Subscriber('/joy_cmd', Twist, joystick_callback)
+                joy =sloppy_joystick()
                 rospy.spin()
 	except rospy.ROSInterruptException:
 		rospy.loginfo("---------- FUSION-ERROR! ---------")
