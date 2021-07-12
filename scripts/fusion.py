8a154ba43dc9d3205403cf5a6dab6a802b1b6097
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-07-12 09:54:53 +0200

    conducted experiments regarding circuit performance

--- scripts/fusion.py
+++ scripts/fusion.py
@@ -29,7 +29,7 @@ class Fusion:
                 
                 This Circuit avoids collisions and assist the joystick using a homing behaviour which is invoked by the joystick
                 '''
-		#cmd_vel.linear.x = bg.AND(10*self.behaviors[1,0],bg.INVOKE(self.behaviors[2,0],self.behaviors[0,0]))
+		#cmd_vel.linear.x  = bg.AND(10*self.behaviors[1,0],bg.INVOKE(self.behaviors[2,0],self.behaviors[0,0]))
                 #cmd_vel.angular.z = bg.PREVAIL(self.behaviors[1,1], bg.OR(self.behaviors[2,1],self.behaviors[0,1]))
 
 
@@ -40,20 +40,37 @@ class Fusion:
                 
                 This Circuit lets the system navigate autonomously except if the joystick provides a different command
                 '''
-		#cmd_vel.linear.x = bg.PREVAIL(self.behaviors[2,0], self.behaviors[0,0])
+		#cmd_vel.linear.x  = bg.PREVAIL(self.behaviors[2,0], self.behaviors[0,0])
                 #cmd_vel.angular.z = bg.PREVAIL(self.behaviors[2,1], bg.PREVAIL(self.behaviors[1,1],self.behaviors[0,1]))
 
                 '''
                 Fusion circuit 3
                 ----------------
                 
-                This Circuit avoids collisions and assist the joystick using a navstack behaviour which is invoked by the joystick
+                This Circuit avoids collisions and assist the joystick using a navstack behaviour which is ORed by the joystick
                 '''
-		cmd_vel.linear.x = bg.AND(10*self.behaviors[1,0],bg.INVOKE(self.behaviors[2,0],self.behaviors[3,0]))
+		cmd_vel.linear.x  = bg.AND(10*self.behaviors[1,0],bg.INVOKE(self.behaviors[2,0],self.behaviors[3,0]))
                 cmd_vel.angular.z = bg.PREVAIL(self.behaviors[1,1], bg.OR(self.behaviors[2,1],self.behaviors[3,1]))
                 
-		self.pub.publish(cmd_vel)
+                '''
+                Fusion circuit 4
+                ----------------
 
+                This Circuit lets the navstack directly control the robot
+                '''
+                #cmd_vel.linear.x  = self.behaviors[3,0]
+                #cmd_vel.angular.z = self.behaviors[3,1]
+
+                '''
+                Fusion circuit 5
+                ----------------
+                
+                This Circuit assist the joystick using a navstack behaviour which is ORed by the joystick
+                '''
+		#cmd_vel.linear.x  =  bg.INVOKE(self.behaviors[2,0],self.behaviors[3,0])
+                #cmd_vel.angular.z =  bg.OR(self.behaviors[2,1],self.behaviors[3,1])
+
+		self.pub.publish(cmd_vel)
 
 
 def EQUIV(x,y):
