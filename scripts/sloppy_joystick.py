71188310a681702225f3b83da5f4530221b43e26
Jan Baumgärtner <22688144+liquidcronos@users.noreply.github.com>
2021-07-13 09:18:01 +0200

    fixed bug in joystick discretization

--- scripts/sloppy_joystick.py
+++ scripts/sloppy_joystick.py
@@ -10,13 +10,12 @@ import behavior_gates as bg
 class sloppy_joystick:
 
     def __init__(self):
-        self.lin_quant_steps = []
-        self.ang_quant_steps = []#[-1.3, -0.7, 0, 0.7, 1.3]
+        self.lin_quant_steps = [-1,0,1]
+        self.ang_quant_steps = [-1.3, -0.7,0, 0.7, 1.3]
         self.noise_variance  = np.array([0.01,0.3])
         self.time_variance   = 2
         self.joystick_input  = np.zeros(2)
 
-        #pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         pub = rospy.Publisher('/sloppy_joy_cmd', Twist, queue_size=1)
         rospy.Subscriber('/joy_cmd', Twist, self.joystick_callback)
         while not rospy.is_shutdown():
@@ -26,10 +25,13 @@ class sloppy_joystick:
             rospy.sleep(wait_time)
 
             joystick_input = self.joystick_input + ctr_noise
+
             if self.lin_quant_steps != []:
-                joystick_input[0]=np.digitize(joystick_input[0],bins=self.lin_quant_steps)
+                bin_value         = np.digitize(joystick_input[0],bins=self.lin_quant_steps)
+                joystick_input[0] = self.lin_quant_steps[bin_value-1]
             if self.ang_quant_steps != []:
-                joystick_input[1]=np.digitize(joystick_input[1],bins=self.ang_quant_steps)
+                bin_value         = np.digitize(joystick_input[1],bins=self.ang_quant_steps)
+                joystick_input[1] = self.ang_quant_steps[bin_value-1]
 
             output = Twist()
             output.linear.x  = max(-1,min(1,joystick_input[0]))
